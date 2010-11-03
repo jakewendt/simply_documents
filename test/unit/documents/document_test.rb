d147b2aa0d0539f360e996cc38526449332289c2
Jake <github@jake.otherinbox.com>
2010-11-03 13:06:10 -0700

    Allowing simply_testable to deduce the model name in testing.

--- test/unit/documents/document_test.rb
+++ test/unit/documents/document_test.rb
@@ -3,13 +3,9 @@ require 'test_helper'
 
 class SimplyDocuments::DocumentTest < ActiveSupport::TestCase
 
-	assert_should_require(:title,
-		:model => 'Document')
-	assert_should_require_attribute_length(:title,
-		:minimum => 4,
-		:model => 'Document')
-	assert_should_belong_to(:owner,:class_name => 'User',
-		:model => 'Document')
+	assert_should_require(:title)
+	assert_should_require_attribute_length(:title, :minimum => 4)
+	assert_should_belong_to(:owner,:class_name => 'User')
 
 	test "should create document" do
 		assert_difference 'Document.count' do
@@ -17,14 +13,6 @@ class SimplyDocuments::DocumentTest < ActiveSupport::TestCase
 			assert !object.new_record?, 
 				"#{object.errors.full_messages.to_sentence}"
 		end
-	end
-
-protected
-
-	def create_object(options = {})
-		record = Factory.build(:document,options)
-		record.save
-		record
 	end
 
 end
