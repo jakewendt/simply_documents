40bbf76081c51d6f61a1fbdd11572233889c2a9f
Jake <github@jakewendt.com>
2011-02-11 10:53:42 -0800

    Updated rails test env to 2.3.11 and cleaned up environment.rb.

--- config/environment.rb
+++ config/environment.rb
@@ -1,7 +1,7 @@
 # Be sure to restart your server when you modify this file
 
 # Specifies gem version of Rails to use when vendor/rails is not present
-RAILS_GEM_VERSION = '2.3.10' unless defined? RAILS_GEM_VERSION
+RAILS_GEM_VERSION = '2.3.11' unless defined? RAILS_GEM_VERSION
 
 # Bootstrap the Rails environment, frameworks, and default configuration
 require File.join(File.dirname(__FILE__), 'boot')
@@ -13,20 +13,15 @@ RAILS_APP_NAME = 'ccls'
 
 Rails::Initializer.run do |config|
 
-	config.gem 'jakewendt-calnet_authenticated'	#,
-#		:lib => 'calnet_authenticated'
-
-	config.gem 'jakewendt-simply_authorized'	#,
-#		:lib => 'simply_authorized'
-
+	config.gem 'jakewendt-calnet_authenticated'
+	config.gem 'jakewendt-simply_authorized'
 	config.gem 'jakewendt-simply_helpful'
+	config.gem 'jakewendt-html_test'
 
 	config.plugin_paths = [
-		File.expand_path(File.join(File.dirname(__FILE__),'../..')),
-		File.expand_path(File.join(File.dirname(__FILE__),'../../..','peter'))
+		File.expand_path(File.join(File.dirname(__FILE__),'../..'))
 	]
-	config.plugins = [:simply_documents,
-		:html_test, :html_test_extension]
+	config.plugins = [:simply_documents]
 
 	config.frameworks -= [:active_resource]
 
